58e84cdd8b9923e15e40a5300ac4bba090d5de74
Davi Kawasaki <davishinjik@gmail.com>
2021-06-04 18:32:55 -0300

    feat: start app w/ routes, pages and keycloak config

---
+++ src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+
+import { useKeycloak } from '@react-keycloak/web'
+import { useState } from 'react'
+
+export default () => {
+  const { keycloak } = useKeycloak()
+  const [showToken, setShowToken] = useState(false)
+
+  const callApi = () => {
+    setShowToken(true)
+    console.log(keycloak?.token)
+    console.log(keycloak?.tokenParsed)
+  }
+
+  return (
+    <div>
+      <div>
+      <div>
+        User is {!keycloak?.authenticated ? 'NOT ' : ''} authenticated</div>
+        {!!keycloak?.authenticated && (
+          <button type="button" onClick={() => keycloak.logout()}>
+            Logout
+          </button>
+        )}
+
+        <button type="button" onClick={callApi}>
+          Show token and details
+        </button>
+      </div>
+      <div>
+        {showToken && (
+          <div style={{padding: '1em'}}>
+            <strong>JWT Token:</strong>
+            <p>{JSON.stringify(keycloak?.token || '')}</p>
+            <strong>JWT content: </strong>
+            <p>{JSON.stringify(keycloak?.tokenParsed || {})}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
