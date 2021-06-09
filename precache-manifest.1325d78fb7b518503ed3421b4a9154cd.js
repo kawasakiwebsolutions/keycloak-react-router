de1bd55741416cf041d5d19cf013021f1b814f6f
davikawasaki <davishinjik@gmail.com>
2021-06-09 11:23:01 +0000

    Deploying to gh-pages from @ kawasakiwebsolutions/keycloak-react-router@331e5befa396823c36e3f12d1458d91e4eb01727 🚀

---
+++ precache-manifest.1325d78fb7b518503ed3421b4a9154cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867d1d969cde48db83151dc1ead1e068",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d482f30177630e1cc220",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3f978074203ebaaf4ebb",
+    "url": "/static/js/2.5c941f61.chunk.js"
+  },
+  {
+    "revision": "e550e927d7542518bb47093c4edf77ac",
+    "url": "/static/js/2.5c941f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d482f30177630e1cc220",
+    "url": "/static/js/main.96c96144.chunk.js"
+  },
+  {
+    "revision": "4c4c78544c337d6a706b",
+    "url": "/static/js/runtime-main.9fcbd102.js"
+  }
+]);
